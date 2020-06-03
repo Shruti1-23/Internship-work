13ecb6808d5be3b5e5f10f0d6fa79f42931158a6
Shruti1-23 <63156464+Shruti1-23@users.noreply.github.com>
2020-06-03 20:59:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Welcome to MY WEBSITE</h1>
+     <p><strong>HOW TO MAKE PANCAKE</strong>
+          <h2>Ingredients (serves 4)</h2>
+
+
+    200g plain flour
+    2 large eggs
+    400ml milk
+    1 tsp vegetable oil
+    Utensils
+
+
+
+Large frying pan
+Whisk
+Large bowl
+Thin spatula
+Prep (less than 30 minutes)
+
+Put the plain flour in a bowl, before using your (clean) fingers to make a well at its centre
+Crack the eggs into a saucer and pour into the well
+Add 400ml of milk into the well
+Beat the mixture with a hand mixer, working to incorporate all the flour until you have a smooth batter
+Cooking (less than 10 minutes)
+
+Put a large frying pan on the hob on a medium heat
+Drizzle vegetable oil and swirl it around until it is hot and the pan evenly coated
+Spoon 2 tbsp of pancake batter in the centre of the pan and swirl around until the base is coated in a thin layer of batter (depending on the size of your pan – you may need to vary the amount of mixture)
+Cook for approximately one minute on each side, flipping them with the spatula – or toss them if you’re feeling brave</p>
+
+     <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/08/25/14/pancakes-banana-american.jpg"/>
+     </div>
+  );
+  }
+export default App;
